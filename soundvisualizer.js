4fe4b15f7bd5aefe1ad704fd37cf9112393f4ab1
acourtin <acourtin@student.42.fr>
2021-03-06 16:01:20 +0100

    made big norminet tunnel

--- soundvisualizer.js
+++ soundvisualizer.js
@@ -14,6 +14,8 @@ var HEIGHT,
 	windowHalfX,
 	windowHalfY;
 
+let meshGroup;
+let meshBody;
 let musicLaunched = false;
 let audioListener;
 let audio;
@@ -38,7 +40,7 @@ function init() {
 	aspectRatio = WIDTH / HEIGHT;
 	fieldOfView = 50;
 	nearPlane = 1;
-	farPlane = 2000;
+	farPlane = 2000000;
 	camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio,
 		nearPlane, farPlane);
 	camera.position.z = 800;
@@ -62,16 +64,22 @@ function init() {
 
 	// MESH
 	const texture = new THREE.TextureLoader().load( 'https://cdn.intra.42.fr/users/medium_' + 'norminet' + '.jpg' );
-	meshGroup = new THREE.Group();
-	meshBody = new THREE.Mesh(
-		new THREE.CircleGeometry(25, 100),
-		new THREE.MeshBasicMaterial({
-			map: texture
-		})
-	);
-	meshBody.receiveShadow = false;
-	meshGroup.add(meshBody);
-	scene.add(meshGroup);
+
+	meshGroup = new Array(3000);
+	meshBody = new Array(meshGroup.length);
+	for (let i = 0 ; i < meshGroup.length ; i++) {
+		meshGroup[i] = new THREE.Group();
+		meshBody[i] = new THREE.Mesh(
+			new THREE.CircleGeometry(25, 50),
+			new THREE.MeshBasicMaterial({
+				map: texture
+			})
+		);
+		meshBody[i].receiveShadow = false;
+		meshGroup[i].add(meshBody[i]);
+		meshGroup[i].position.set(Math.cos(i) * 300, Math.sin(i) * 300, (-i * 10) + 300)
+		scene.add(meshGroup[i]);
+	}
 }
 
 //LOGIC FUNCTIONS
@@ -79,16 +87,18 @@ function loop() {
 	const data = audioAnalyser.getFrequencyData();
 	if (musicLaunched == true) {
 		const min = 1;
-		const power = .0001;
-
+		const power = .00005;
 		let xData = 0;
-		let yData = 0;
 
-		for (let i = 0 ; i < 500 ; i++) {
-			xData += data[i + 300];
-			yData += data[i];
+		for (let j = 0 ; j < 200 ; j++) {
+			xData += data[j * 5];
 		}
-		meshGroup.scale.set(min + (xData * power), min + (yData * power), .1);
+		meshGroup[0].scale.set(min + (xData * power), min + (xData * power), .1);
+		for (let i = meshGroup.length - 1 ; i > 0 ; i--) {
+			meshGroup[i].scale.set(meshGroup[i - 1].scale.x,
+				meshGroup[i - 1].scale.y,
+				meshGroup[i - 1].scale.z);
+		}
 	}
 	renderer.render(scene, camera);
 	requestAnimationFrame(loop);
