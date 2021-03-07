876e9ac45059d8d8c191eeaad44f1110c2e3699b
AlexandreCourtin <acourtin@student.42.fr>
2021-03-07 10:40:02 +0100

    added index for future pages

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 })
 
+app.get('/page1', (req, res) => {
+	res.sendFile(__dirname + "/page1.html");
+})
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
